e3d56f3f45591b9312318696a082f9b48f85c6d1
Charles Ouverleaux <33698505+CharlesOuverleaux@users.noreply.github.com>
2021-05-18 15:58:47 +0200

    added delete method

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,17 @@ Rails.application.routes.draw do
   get 'tasks/new', to: 'tasks#new'
   # CREATE a task based on the new input
   post 'tasks', to: 'tasks#create'
+
   # READ display all tasks on the index page
   get 'tasks', to: 'tasks#index'
   # READ display id page for each task with its details
   get 'tasks/:id', to: 'tasks#show', as: :task
+
+  # UPDATE edit a task, creates the page where you can edit the task
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :task_edit
+  # UPDATE, how you update the task
+  patch 'tasks/:id', to: 'tasks#update'
+
+  # DELETE, how you delete a task
+  delete 'tasks/:id', to: 'tasks#destroy', as: :task_destroy
 end
